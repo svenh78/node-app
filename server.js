a9214166826f2f6b90ca7efe22e6d1cd0416957b
Sven Harmes <s.harmes@uniserver.nl>
2025-05-02 15:06:39 +0200

    testing 123

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const port = 3000;
 const server = createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, world -> 2345!');
+  res.end('Hello, world -> 12345!');
 });
 
 server.listen(port, hostname, () => {
