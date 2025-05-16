bdbf5cd8b28f5203bdec6a4f5e30f5ec75c7dd75
Sven Harmes <s.harmes@uniserver.nl>
2025-05-16 10:52:34 +0200

    testing the github self-hosted runner

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const port = 3000;
 const server = createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Node app testing!');
+  res.end('Node app, testing the github self-hosted runner');
 });
 
 server.listen(port, hostname, () => {
